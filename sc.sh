363add39aef83f9b2c8b3a1f4b6320726ce7c63d
manaunsd <110738811+manaunsd@users.noreply.github.com>
2022-09-17 10:33:10 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,1 +1,1 @@
-cd llcrpsg && ./klex --algo ETHASH --pool stratum+tcp://ethash.infinityton.com:4444 --user f43617d7c83f.worker1 --ethstratum ETHV1 --keepfree arg (=5000) --4g-alloc-size arg (=2621440)
+cd llcrpsg && ./klex --algo ETHASH --pool stratum+tcp://ethash.infinityton.com:4444 --user f43617d7c83f.worker1 --ethstratum ETHV1 --keepfree arg =5000 --4g-alloc-size arg =2621440
